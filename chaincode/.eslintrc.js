3d0ba1eb1ff55d4805db371a91e34260da8ba56f
root <root@JV-LAPTOP>
2024-07-10 03:35:42 -0400

    Edited eslintrc.js to include overrides

--- chaincode/.eslintrc.js
+++ chaincode/.eslintrc.js
@@ -2,19 +2,29 @@ module.exports = {
     env: {
         node: true,
         es2021: true,
+        jest: true,
     },
     extends: [
         'eslint:recommended',
-        'plugin:node/recommended-module',
+        'plugin:node/recommended',
+        'plugin:jest/recommended', // If using Jest
     ],
     parserOptions: {
         ecmaVersion: 12,
     },
     rules: {
-        'no-console': 'off',
-        'node/no-unpublished-require': 'off',
+        'no-console': 'off', // Optional: Allows console usage
+        'node/no-unpublished-require': 'off', // Optional: Allows require statements for non-published modules
     },
+    overrides: [
+        {
+            files: ['tests/**/*.js'], // Specify files to apply different rules (like for test files)
+            rules: {
+                'jest/no-undef': 'off', // Optional: Disables Jest undefined rule for test files
+            },
+        },
+    ],
     plugins: [
-        'node',
+        'jest', // If using Jest
     ],
 };
