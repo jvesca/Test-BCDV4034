4eb19a85374528fd691eba7208d1474f034bf22c
root <root@JV-LAPTOP>
2024-07-12 00:53:38 -0400

    Edited API Workflow and addToWallet.Js

--- application-gateway/addToWallet.js
+++ application-gateway/addToWallet.js
@@ -4,7 +4,11 @@ const fs = require('fs');
 const path = require('path');
 const { Wallets } = require('fabric-network');
 
-const testNetworkRoot = path.resolve(require('os').homedir(), 'fabric-samples/test-network');
+const testNetworkRoot = process.env.GITHUB_WORKSPACE 
+    ? path.resolve(process.env.GITHUB_WORKSPACE, 'fabric-samples/test-network')
+    : path.resolve(require('os').homedir(), 'fabric-samples/test-network');
+
+console.log('testNetworkRoot:', testNetworkRoot);
 
 async function main() {
 
