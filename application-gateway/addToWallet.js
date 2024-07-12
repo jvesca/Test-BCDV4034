7d4cb0d2d0be3b043febf59c7b07a3fe6d6d6d5b
root <root@JV-LAPTOP>
2024-07-12 01:40:07 -0400

    Modified addToWallet.js to include logs

--- application-gateway/addToWallet.js
+++ application-gateway/addToWallet.js
@@ -11,9 +11,9 @@ const testNetworkRoot = process.env.GITHUB_WORKSPACE
 console.log('testNetworkRoot:', testNetworkRoot);
 
 async function main() {
-
     try {
         const wallet = await Wallets.newFileSystemWallet('./wallet');
+        console.log('Wallet path:', path.resolve('./wallet'));
         
         const predefinedOrgs = [
             {
@@ -33,13 +33,18 @@ async function main() {
 
         for (const org of predefinedOrgs) {
             const credPath = path.join(testNetworkRoot, '/organizations/peerOrganizations/', org.name, '/users');
+            console.log('Checking credPath:', credPath);
             
             for (const user of org.users) {
                 const mspFolderPath = path.join(credPath, `${user}@${org.name}`, '/msp');
+                console.log('Checking mspFolderPath:', mspFolderPath);
                 
                 // expecting only one cert file and one key file to be in the directories
                 const certFile = path.join(mspFolderPath, '/signcerts/', fs.readdirSync(path.join(mspFolderPath, '/signcerts'))[0]);
                 const keyFile = path.join(mspFolderPath, '/keystore/', fs.readdirSync(path.join(mspFolderPath, '/keystore'))[0]);
+
+                console.log('certFile:', certFile);
+                console.log('keyFile:', keyFile);
 
                 const cert = fs.readFileSync(certFile).toString();
                 const key = fs.readFileSync(keyFile).toString();
@@ -55,8 +60,11 @@ async function main() {
 
                 const identityLabel = `${user}@${org.name}`;
                 await wallet.put(identityLabel, identity);
+                console.log(`Successfully added ${identityLabel} to the wallet`);
             }
         }
+
+        console.log('All identities have been successfully added to the wallet');
 
     } catch (error) {
         console.log(`Error adding to wallet. ${error}`);
@@ -65,9 +73,9 @@ async function main() {
 }
 
 main().then(() => {
-    console.log('done');
+    console.log('Wallet population completed successfully');
 }).catch((e) => {
-    console.log(e);
+    console.log('Failed to populate wallet:', e);
     console.log(e.stack);
     process.exit(-1);
 });
