a1110847bc9ba6be6896a9c2fd1e13fdac1ad846
root <root@JV-LAPTOP>
2024-07-13 23:52:05 -0400

    Modified App.test.js and React Frontend Workflow

--- react-frontend/aquatrustchain/src/App.test.js
+++ react-frontend/aquatrustchain/src/App.test.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import { act } from 'react-dom/test-utils';
 import App from './App';
 
-test('renders AquaTrustChain in Navigation', async () => {
-  await act(async () => {
-    render(<App />);
-  });
+test('renders AquaTrustChain in Navigation', () => {
+  render(<App />);
   const brandElement = screen.getByText(/AquaTrustChain/i);
   expect(brandElement).toBeInTheDocument();
 });
