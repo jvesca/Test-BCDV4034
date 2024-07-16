e1decb4aab7e4bcf094b06f7ac2316bef73af53e
root <root@JV-LAPTOP>
2024-07-16 01:17:06 -0400

    Modified Chaincode Workflow file

--- chaincode/.eslintrc.js
+++ chaincode/.eslintrc.js
@@ -1,32 +1,30 @@
 module.exports = {
     env: {
-        node: true,
-        es2021: true,
-        jest: true,
+        node: true,          //  code runs in a Node.js environment
+        es2021: true,        //  using ECMAScript 2021 features
+        jest: true,          //  using Jest for testing
     },
     extends: [
-        'eslint:recommended',
-        'plugin:node/recommended',
-        'plugin:jest/recommended', // If using Jest
+        'eslint:recommended',          // Use recommended ESLint rules
+        'plugin:node/recommended',     // Use recommended rules for Node.js
+        'plugin:jest/recommended',     // Use recommended rules for Jest
     ],
     parserOptions: {
-        ecmaVersion: 12,
+        ecmaVersion: 12,     // Use ECMAScript 2021 syntax
     },
     rules: {
-        'no-console': 'off', // Optional: Allows console usage
-        'node/no-unpublished-require': 'off', // Optional: Allows require statements for non-published modules
-        // 'no-unused-vars': ['error', { 'varsIgnorePattern': 'allocationObjType'}], // Ignore specific unused variable
-
+        'no-console': 'off', // Allow console.log statements
+        'node/no-unpublished-require': 'off', // Allow require statements for non-published modules
     },
     overrides: [
         {
-            files: ['tests/**/*.js'], // Specify files to apply different rules (like for test files)
+            files: ['tests/**/*.js'], // Specific rules for test files
             rules: {
-                'jest/no-undef': 'off', // Optional: Disables Jest undefined rule for test files
+                'jest/no-undef': 'off', // Disable Jest undefined rule for test files
             },
         },
     ],
     plugins: [
-        'jest', // If using Jest
+        'jest', // Jest plugin
     ],
 };
